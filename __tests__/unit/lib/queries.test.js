b6a7135a3411cd49a332ac6d8ca8f6dec172db6d
David Thompson <davidt@hey.com>
2021-07-01 11:00:45 +0100

    Move the tests inside the describe block!

--- __tests__/unit/lib/queries.test.js
+++ __tests__/unit/lib/queries.test.js
@@ -25,56 +25,59 @@ describe('Calling filterAges ', () => {
       ],
     })
   })
-})
 
-it('should return a query containing gte the max_age supplied', () => {
-  const query = Queries.filterAges({}, { query: { max_age: 12 } })
-  expect(query).toStrictEqual({
-    "$and": [
-      {
-        "$or": [
-          {
-            "max_age": null,
-          },
-          {
-            "max_age": {
-              "$gte": 12,
+  // TODO: I think this should be gte min_age 0 and lte max_age 12 
+  it('should return a query containing gte the max_age supplied', () => {
+    const query = Queries.filterAges({}, { query: { max_age: 12 } })
+    expect(query).toStrictEqual({
+      "$and": [
+        {
+          "$or": [
+            {
+              "max_age": null,
             },
-          },
-        ],
-      },
-    ],
+            {
+              "max_age": {
+                "$gte": 12,
+              },
+            },
+          ],
+        },
+      ],
+    })
+  })
+
+  // TODO I think this should be lte max_age && gte min_age
+  it('should return a query containing gte the max_age and lte the min_age supplied', () => {
+    const query = Queries.filterAges({}, { query: { min_age: 8, max_age: 14 } })
+    expect(query).toStrictEqual({
+      "$and": [
+        {
+          "$or": [
+            {
+              "min_age": null,
+            },
+            {
+              "min_age": {
+                "$lte": 8,
+              },
+            },
+          ],
+        },
+        {
+          "$or": [
+            {
+              "max_age": null,
+            },
+            {
+              "max_age": {
+                "$gte": 14,
+              },
+            },
+          ],
+        },
+      ],
+    })
   })
 })
 
-it('should return a query containing gte the max_age and lte the min_age supplied', () => {
-  const query = Queries.filterAges({}, { query: { min_age: 8, max_age: 14 } })
-  expect(query).toStrictEqual({
-    "$and": [
-      {
-        "$or": [
-          {
-            "min_age": null,
-          },
-          {
-            "min_age": {
-              "$lte": 8,
-            },
-          },
-        ],
-      },
-      {
-        "$or": [
-          {
-            "max_age": null,
-          },
-          {
-            "max_age": {
-              "$gte": 14,
-            },
-          },
-        ],
-      },
-    ],
-  })
-})
