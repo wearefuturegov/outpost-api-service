64d7259542d4c0018fd8ca583afb2f80dfbdd9cd
Han <apricot13@users.noreply.github.com>
2024-06-06 17:29:56 +0100

    missing await

--- src/controllers/v1/services/index.js
+++ src/controllers/v1/services/index.js
@@ -74,7 +74,7 @@ module.exports = {
   show: async (req, res, next) => {
     try {
       logger.info(req.params)
-      const query = getService.buildQuery(req.params)
+      const query = await getService.buildQuery(req.params)
       const result = await getService.executeQuery(query)
       // let result = await db()
       //   .collection("indexed_services")
